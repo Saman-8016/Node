d9b919e6e1935f5f7655f6bfabd4c1de95664b6f
Saman <83778434+Saman-8016@users.noreply.github.com>
2021-05-18 23:22:14 -0600

    updated

--- Prime_flix/app.js
+++ Prime_flix/app.js
@@ -51,6 +51,10 @@ app.post("/movies", (req, res) => {
     res.redirect("/movies")
 })
 
+app.get("/login", (req, res) => {
+    res.render("/login")
+})
+
 app.get("*", (req, res) => {
     res.render("error")
 })
